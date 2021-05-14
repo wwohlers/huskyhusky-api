f6b05b32e081c1492b09d1168380eebab56f0af7
Bill <wohlers.w@husky.neu.edu>
2021-05-14 17:54:27 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 
   if (module.hot) {
